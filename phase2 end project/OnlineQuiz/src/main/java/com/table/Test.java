36d30e3368eb7c20a689b971cbb7a4caea457d8a
KARTHIK DAGGUMATI <dvsk1269@gmail.com>
2024-01-19 19:04:51 +0530

    phase2 project

---
+++ phase2 end project/OnlineQuiz/src/main/java/com/table/Test.java
@@ -0,0 +1,10 @@
+package com.table;
+
+public class Test 
+{
+	public String title;
+	public String name;
+	public String category;
+	public int score;
+
+}
---
+++ phase2 end project/OnlineQuiz/src/main/java/com/table/User.java
@@ -0,0 +1,8 @@
+package com.table;
+
+public class User
+{
+	public String name;
+	public String email;
+	public String password;
+}
